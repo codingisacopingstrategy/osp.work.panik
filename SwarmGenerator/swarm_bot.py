f7970e7a92f7bdda171c808b234ddf7657e7caf6
codingisacopingstrategy <eric@ericschrijver.nl>
2011-11-03 09:24:36 +0100

    It works with text

--- SwarmGenerator/swarm_bot.py
+++ SwarmGenerator/swarm_bot.py
@@ -26,12 +26,28 @@ def swarm_bot(category, text=None):
     
     points = [((i**2 % 25), i**2 / 25) for i in range(0, upto)]
     
-    bot.size(24 * scale, 38 * scale );
+    HEIGHT = 38 * scale
+    WIDTH = 24 * scale
+    bot.size(WIDTH, HEIGHT);
     
+    if text:
+        bot.align(bot.RIGHT)
+        bot.fontsize(36)
+        twidth, theight = bot.textmetrics(text, 392)
+        bot.text(text, 20, HEIGHT - theight + 7.2, 392)
+        bot.nofill()
+        bot.stroke(0)
+        bot.strokewidth(17)
+        bot.beginpath(WIDTH - twidth - 30, HEIGHT - theight - 30 )
+        bot.lineto(WIDTH - 10, HEIGHT - theight - 30)
+        bot.lineto(WIDTH - 10, HEIGHT - 10)
+        bot.lineto(WIDTH - twidth - 30, HEIGHT - 10)
+        bot.endpath()
+
     for i in range(0, upto):
         bot.image(imgs[i], points[i][0] * scale * random(), points[i][1] * scale)
     
     bot._canvas.flush(frame=0)
 
 if __name__ == "__main__":
-    swarm_bot("Category:Clothing_illustrations")
+    swarm_bot("Category:Clothing_illustrations", text="PANIK")
